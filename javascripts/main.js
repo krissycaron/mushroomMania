f1ee2ca84ec5a92e79ea1a499802e269c5f535d0
Kristin Caron <krissycaron@gmail.com>
2017-05-15 13:14:19 -0500

    error firebase is not connected

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,10 +1,43 @@
 console.log("main js connected")
 
-// app.run((FIREBASE_CONFIG) => {
-//    firebase.initializeApp(FIREBASE_CONFIG);
-// });
+app.run((FIREBASE_CONFIG) => {
+   firebase.initializeApp(FIREBASE_CONFIG);
+});
 
-app.controller("itemControl", ()=>{
-	console.log("in item control")
-	
-})
+app.controller("mushroomCtrl", ($http, $q, $scope, FIREBASE_CONFIG) => {
+  let getItemList = () => {
+  let itemz = [];
+  
+    return $q((resolve, reject) => {
+      $http.get(`${FIREBASE_CONFIG.databaseURL}/mushrooms.json`)
+      .then((fbItems)=> {
+        let itemCollection = fbItems.data;
+        Object.keys(itemCollection).forEach((key) => {
+            itemCollection[key].id=key;
+            itemz.push(itemCollection[key]);
+          });
+          console.log("resultz", itemz);
+          resolve(itemz);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+    });
+  };
+
+  let getItems = () => {
+      getItemList().then((itemz)=>{
+        $scope.items = itemz;
+      }).catch((error)=>{
+        console.log("got and error", error);
+      });
+    };
+
+  getItems();
+
+
+});
+
+
+
+
